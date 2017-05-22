bcd8928bbfbfa51fa8fb8872a9be4591cdcee964
Graham R Jeffries <graham.r.jeffries@gmail.com>
2017-05-22 14:21:24 -0400

    expand col. names for rds objects

--- src/ingest.R
+++ src/ingest.R
@@ -14,33 +14,44 @@ unzip_list <- unzip(dl_path, exdir = 'data/source/', junkpaths = TRUE)
 
 # state
 state <- readOGR('data/source/BRUFE250GC_SIR.shp', verbose = FALSE)
-names(state) <- c('state_nm', 'region_nm', 'state_cd')
-state$state_nm <- str_to_lower(state$state_nm)
-state$region_nm <- str_to_lower(state$region_nm)
+names(state) <- c('state_name', 'region_name', 'state_code')
+state$state_name <- str_to_lower(state$state_name)
+state$region_name <- str_to_lower(state$region_name)
 # clgeo_IsValid(state)
-writeOGR(state, 'data/brazil_state.shp', 'state', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
 saveRDS(state, 'data/brazil_state.rds')
+names(state) <- c('state_nm', 'region_nm', 'state_cd')
+writeOGR(state, 'data/brazil_state.shp', 'state', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
+
 
 # meso
 meso <- readOGR('data/source/BRMEE250GC_SIR.shp', verbose = FALSE)
-names(meso) <- c('meso_nm', 'meso_cd')
-meso$meso_nm <- str_to_lower(meso$meso_nm)
+names(meso) <- c('meso_name', 'meso_code')
+meso$meso_name <- str_to_lower(meso$meso_name)
 # clgeo_IsValid(meso)
-writeOGR(meso, 'data/brazil_meso.shp', 'meso', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
 saveRDS(meso, 'data/brazil_meso.rds')
+names(meso) <- c('meso_nm', 'meso_cd')
+writeOGR(meso, 'data/brazil_meso.shp', 'meso', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
+
 
 # micro
 micro <- readOGR('data/source/BRMIE250GC_SIR.shp', verbose = FALSE)
-names(micro) <- c('micro_nm', 'micro_cd')
-micro$micro_nm <- str_to_lower(micro$micro_nm)
+names(micro) <- c('micro_name', 'micro_code')
+micro$micro_name <- str_to_lower(micro$micro_name)
 # clgeo_IsValid(micro)
-writeOGR(micro, 'data/brazil_micro.shp', 'micro', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
 saveRDS(micro, 'data/brazil_micro.rds')
+names(micro) <- c('micro_nm', 'micro_cd')
+writeOGR(micro, 'data/brazil_micro.shp', 'micro', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
+
 
 # muni
 muni <- readOGR('data/source/BRMUE250GC_SIR.shp', verbose = FALSE)
-names(muni) <- c('muni_nm', 'muni_cd')
-muni$muni_nm <- str_to_lower(muni$muni_nm)
+names(muni) <- c('muni_name', 'muni_code')
+muni$muni_name <- str_to_lower(muni$muni_name)
 # clgeo_IsValid(muni)
-writeOGR(muni, 'data/brazil_muni.shp', 'muni', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
 saveRDS(muni, 'data/brazil_muni.rds')
+names(muni) <- c('muni_nm', 'muni_cd')
+writeOGR(muni, 'data/brazil_muni.shp', 'muni', overwrite_layer = TRUE, driver = 'ESRI Shapefile')
+
+
+
+
